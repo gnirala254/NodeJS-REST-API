7c36fa923ebea65f2a4461fecd4ba04e6726663b
Gautam-Nirala <gnirala254@gmail.com>
2020-08-27 11:59:23 +0530

    added file upload property and public route for vieving image

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ mongoose.connect(connString, {
 mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
+app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
